9e06bdfe5b6729e29febac163d04b914339b4796
IvanyutinSA <ivanyutin@gmail.com>
2024-02-16 15:35:09 +0600

    fixed dichotomy and fibonacci

--- algorithms/one_dimentional.py
+++ algorithms/one_dimentional.py
@@ -1,48 +1,46 @@
-def dichotomy(f, a, b, e=.00001):
+def dichotomy(f, a, b, e=.0000001):
 
-    while b-a < 2*e:
+    while round(b-a, 7) > 2*e:
         x = (a+b)/2
         x1 = x-e
         x2 = x+e
 
         if (f(x1) < f(x2)):
-            a = x1
-        else:
             b = x2
+        else:
+            a = x1
 
     return f((a+b)/2)
 
-
-def fibonacci(f, a, b, n = 1000):
+def fibonacci(f, a, b, e=.0000001):
     F = [0, 1, 1]
-    
-    for _ in range(n-2):
+
+    while F[-1] < (b-a)/e:
         F.append(F[-1] + F[-2])
 
-    L = b-a
-    x1 = F[n-1]/F[n]*L
-    x2 = b - F[n-1]/F[n]*L
-
-    f1 = f(x1)
-    f2 = f(x2)
+    n = len(F) - 1
+    f1, f2 = float('inf'), float('inf')
 
     while n > 2:
+        L = b-a 
+        x1 = a + F[n-1]/F[n]*L
+        x2 = b - F[n-1]/F[n]*L
+        f1, f2 = f(x1), f(x2)
         if f2 < f1:
             b = x1
             f1 = f2
             x1 = x2
             L = b-a
-
-            x2 = b - F[n-2]/F[n-1]*L
+            x2 = a+(b-x1)
             f2 = f(x2)
         else:
             a = x2
             f2 = f1
             x2 = x1
             L = b-a
-            x1 = a + F[n-2]/F[n-1]*L
+            x1 = b-(x2-a)
             f1 = f(x1)
-        n-=1 
+        n -= 1
 
     return min(f1, f2)
 
